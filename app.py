eb5a28f793bcd91d70d0d8b8c1f5ebc6aee557b0
usrsem <user7em@gmail.com>
2021-11-30 03:00:09 +0300

    --amend

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 import random
 
